79097d72b16e0993f2ecf521262e2e68fa14f74b
Habib Ur Rehman <haburrehman@eand.com>
2024-01-23 12:58:18 +0500

    -removed EasyInsurance file

--- Sources/SmilesEasyInsurrance/ViewControllers/EasyInsuranceVC/EasyInsuranceVC.swift
+++ Sources/SmilesEasyInsurrance/ViewControllers/EasyInsuranceVC/EasyInsuranceVC.swift
@@ -12,7 +12,7 @@ class EasyInsuranceVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       
     }
 
 
